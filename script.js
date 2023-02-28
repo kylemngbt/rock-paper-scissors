298c650d59a385b58a905a47c9b253158942f85b
Kyle Mangabat <kyle.mngbt@gmail.com>
2023-02-28 16:03:34 +0800

    Create initial functions

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello");
+function getComputerChoice(){
+    return Math.floor((Math.random() * 3) + 1);
+}
+
+function playRound(playerSelection, computerSelection){
+    return
+}
+
+function game(){
+    /*
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+    */
+}
+
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("Choose your move: ");
+
