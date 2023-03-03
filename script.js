b9eb111812a306a4ebee2c939f8932f8447c4e28
Kyle Mangabat <kyle.mngbt@gmail.com>
2023-03-03 14:22:16 +0800

    Remove the turnNumberToString function
    Removed it as it became obsolete with the changes made on getComputerChoice function

--- script.js
+++ script.js
@@ -11,24 +11,10 @@ function getComputerChoice(){
     }
 }
 
-function turnNumberToString(value){
-    if (value === 1) {
-        return 'rock';
-    } 
-    else if (value === 2){
-        return 'paper';
-    } 
-    else if (value === 3){
-        return 'scissors';
-    }
-}
-
 function playRound(playerSelection, computerSelection){
-    computerSelection = turnNumberToString(computerSelection);
-    console.log(computerSelection);
     if (playerSelection === 'rock' && computerSelection === 'paper'){
-        console.log("You lost! The computer chose Paper. \n Paper beats Rock");
-        return false;  
+        return "You lost! The computer chose Paper. \n Paper beats Rock";
+        
     } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
         console.log("You win! The computer chose Scissors. \n Rock beats Scissors");
         return true;
